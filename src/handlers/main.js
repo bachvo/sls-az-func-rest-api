52599651fd310dc1f6596c5e725cad071e811167
Bach Vo <bach_vo@hotmail.com>
2020-03-29 21:02:51 -0700

    update function to call azure storage

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -1,12 +1,46 @@
-const model = require("../data.json");
+const model = require('../data.json');
+const storage = require('azure-storage');
+const table = 'https://portfolio-db.table.cosmos.azure.com:443/';
+const accountKey = 'eXeZt0qrvSEnAZgKxBzBaaFcZZtlo9hr4DEFQH6dW3HWxtgunYfXUONtsLrthnH0OfF85EACQkVY2wQQphZtuA==';
+const accountName = 'portfolio-db';
+const protocol = 'https';
+const connectionString = `DefaultEndpointsProtocol=${protocol};AccountName=${accountName};AccountKey=${accountKey};TableEndpoint=${table};`;
+const storageClient = storage.createTableService(connectionString);
 
-module.exports.handler = async function(context) {
-  context.log('JavaScript HTTP trigger function processed a request.');
+const filterOut = [
+  'PartitionKey',
+  'RowKey',
+  'Timestamp',
+  '.metadata',
+]
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: {
-      data: model,
-    },
-  };
+function sanitizeResults(result) {
+  const entity = { ...result.entries[0] };
+  filterOut.forEach(filterField => {
+    delete entity[filterField];
+  })
+
+  const entityKeys = Object.keys(entity);
+  const finishResult = entityKeys.reduce((obj, data) => {
+    obj[data] = entity[data]._
+    return obj;
+  }, {});
+
+  return finishResult;
+}
+
+module.exports.handler = function(context) {
+  const query = new storage.TableQuery().where('PartitionKey eq ?', 'profile');
+  
+  storageClient.queryEntities('portfolio', query, null, function(error, result, response) {
+    const _model= { ...model };
+    const profileFields = sanitizeResults(result);
+  
+    Object.assign(_model.profile, profileFields)
+    context.res = {
+      status: 200,
+      body: _model
+    };
+    context.done()
+  })
 };
